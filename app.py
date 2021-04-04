8a0b3b54d61dc063da6473ec62b1fe90dd1bf931
Matan Budimir <matanbudimir@gmail.com>
2021-04-04 20:24:33 +0200

    use gpio

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, Blueprint
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import config
 
 app = Flask(__name__)
 
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(config.channels(), GPIO.OUT)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(config.channels(), GPIO.OUT)
 
 
 @app.route('/')
 def hello_world():
-#    GPIO.output(18, not GPIO.input(18))
+    GPIO.output(18, not GPIO.input(18))
     return 'Hello World!'
 
 
