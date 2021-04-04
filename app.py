fd40ac45d49f46cb504ceac11f1bd37724b08576
Matan Budimir <matanbudimir@gmail.com>
2021-04-04 20:22:28 +0200

    please work

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, Blueprint
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import config
 
 app = Flask(__name__)
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(config.channels(), GPIO.OUT)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(config.channels(), GPIO.OUT)
 
 
 @app.route('/')
 def hello_world():
-    GPIO.output(18, not GPIO.input(18))
+#    GPIO.output(18, not GPIO.input(18))
     return 'Hello World!'
 
 
-if __name__ == '__main__':
-    app.run(debug=config.DEBUG, port=config.PORT, host=config.HOST, threaded=True)
+app.run(debug=config.DEBUG, port=config.PORT, host=config.HOST, threaded=True)
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 def channels() -> list:
     return [18]
 
+
 HOST = '0.0.0.0'
 DEBUG = False
-PORT = 80
+PORT = 5000
