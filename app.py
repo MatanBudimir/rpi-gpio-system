f651c2c49e7083a3fbd2384de2003d8d3f160cc7
Matan Budimir <matanbudimir@gmail.com>
2021-04-04 20:58:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,16 @@ GPIO.setup(config.channels(), GPIO.OUT, initial=GPIO.HIGH)
 def gpio_switch(gpio_id: int):
     try:
         GPIO.output(gpio_id, not GPIO.input(gpio_id))
-        return jsonify({'success': True, 'message': f'{config.CHANNELS[gpio_id]["name"]} was turned on.' if not GPIO.input(gpio_id) else 'Turned off.'})
+        return jsonify({'success': True, 'message': f'{config.CHANNELS[gpio_id]["name"]} was turned on.' if not GPIO.input(gpio_id) else f'{config.CHANNELS[gpio_id]["name"]} was turned off.'})
     except Exception as exception:
         return jsonify({'success': False, 'message': exception.__str__()})
 
+
+@app.route('/output/register', methods=['POST'])
+def gpio_register():
+    data = request.get_json()
+
+    return data
 
 try:
     app.run(debug=config.DEBUG, port=config.PORT, host=config.HOST, threaded=True)
